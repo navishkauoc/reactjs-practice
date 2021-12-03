c9414472f2a791a2f1a77d0d05113647a274e22a
navishkauoc <navishka.randika@gmail.com>
2021-12-04 00:27:02 +0530

    Removing completed tasks

--- 02) todo-typescript-crud/todo-list/src/App.tsx
+++ 02) todo-typescript-crud/todo-list/src/App.tsx
@@ -23,6 +23,14 @@ const App: FC = () => {
     setDeadLine(0);
   };
 
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
   return (
     <div className="App">
       <div className="header">
@@ -47,7 +55,7 @@ const App: FC = () => {
 
       <div className="todoList">
         {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task} />;
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })}
       </div>
     </div>
