952f0ddc70decc4d5181ea39d98eaaf073da9cce
navishkauoc <navishka.randika@gmail.com>
2022-01-18 23:20:02 +0530

    Socket.io configs

--- 03) real-time-chat-app/socket-io-chat-app/server/index.js
+++ 03) real-time-chat-app/socket-io-chat-app/server/index.js
@@ -7,6 +7,21 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+    cors : {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+})
+
 server.listen(3001, () => {
     console.log("SERVER RUNNING");
 })
