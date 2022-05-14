c6c5d23910567da3489e0e46eb7fb78636f97b2b
navishkauoc <navishka.randika@gmail.com>
2022-05-14 23:31:26 +0530

    Styles added

--- 04) task-tracker/task-tracker/src/components/Header.js
+++ 04) task-tracker/task-tracker/src/components/Header.js
@@ -2,8 +2,9 @@ import PropTypes from 'prop-types'
 
 const Header = ({title}) => {
   return (
-    <header>
-      <h1 style={headingStyle}>{title}</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <button className='btn'>Add</button>
     </header>
   )
 }
@@ -16,9 +17,10 @@ Header.propTypes = {
   title: PropTypes.string,
 }
 
-const headingStyle = {
-  color: 'red',
-  backgroundColor: 'black'
-}
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
 
 export default Header
