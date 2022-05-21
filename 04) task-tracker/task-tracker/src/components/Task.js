e32e70ac622308dc3a0f37e5a413950a27953e30
navishkauoc <navishka.randika@gmail.com>
2022-05-21 22:27:13 +0530

    Delete button added with react-icons

--- 04) task-tracker/task-tracker/src/components/Task.js
+++ 04) task-tracker/task-tracker/src/components/Task.js
@@ -1,10 +1,12 @@
-const Task = ({task}) => {
-  return (
-    <div className="task">
-        <h3>{task.text}</h3>
-        <p>{task.day}</p>
-    </div>
-  )
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task }) => {
+    return (
+        <div className="task">
+            <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} /></h3>
+            <p>{task.day}</p>
+        </div>
+    )
 }
 
 export default Task
