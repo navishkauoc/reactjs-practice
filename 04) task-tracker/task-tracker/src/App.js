d122d5f8c9123939bdde80ac18a7318c70f71bde
navishkauoc <navishka.randika@gmail.com>
2022-05-24 23:56:20 +0530

    Optional message added if there are no tasks

--- 04) task-tracker/task-tracker/src/App.js
+++ 04) task-tracker/task-tracker/src/App.js
@@ -33,7 +33,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks to Show')}
     </div>
   );
 }
