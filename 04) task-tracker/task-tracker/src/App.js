58848039680c97c2f35acdd1027e47ba025ff156
navishkauoc <navishka.randika@gmail.com>
2022-05-29 15:24:38 +0530

    Toggle reminder & conditional styling

--- 04) task-tracker/task-tracker/src/App.js
+++ 04) task-tracker/task-tracker/src/App.js
@@ -30,10 +30,15 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks to Show')}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
     </div>
   );
 }
